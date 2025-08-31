61a9255b36d52d77ba3280030695777eebb57839
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-08-31 07:09:25 -0400

    Add model for gemstones

---
+++ src/main/java/dev/sbs/minecraftapi/skyblock/data/json/JsonGemstone.java
@@ -0,0 +1,35 @@
+package dev.sbs.minecraftapi.skyblock.data.json;
+
+import com.google.gson.annotations.SerializedName;
+import dev.sbs.api.collection.concurrent.Concurrent;
+import dev.sbs.api.collection.concurrent.ConcurrentMap;
+import dev.sbs.api.data.json.JsonModel;
+import dev.sbs.api.data.json.Resource;
+import dev.sbs.minecraftapi.skyblock.data.Gemstone;
+import dev.sbs.minecraftapi.text.ChatFormat;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@Resource(
+    path = "skyblock",
+    name = "gemstones"
+)
+@NoArgsConstructor(access = AccessLevel.NONE)
+public class JsonGemstone implements Gemstone, JsonModel {
+
+    private @Id @NotNull String id = "";
+    private @NotNull String name = "";
+    private @NotNull String symbol = "";
+    private @NotNull ChatFormat format = ChatFormat.WHITE;
+    @SerializedName("stat")
+    private @NotNull String statId = "";
+    private @NotNull ConcurrentMap<Type, ConcurrentMap<ChatFormat, Double>> values = Concurrent.newMap();
+
+}
