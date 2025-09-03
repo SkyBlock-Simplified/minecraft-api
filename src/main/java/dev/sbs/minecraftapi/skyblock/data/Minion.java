409b251a24186a92046c3904d0701bcfffcb9531
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-09-03 14:02:08 -0400

    Add prelim unique crafts and pet score data

--- src/main/java/dev/sbs/minecraftapi/skyblock/data/Minion.java
+++ src/main/java/dev/sbs/minecraftapi/skyblock/data/Minion.java
@@ -1,6 +1,7 @@
 package dev.sbs.minecraftapi.skyblock.data;
 
 import com.google.gson.annotations.SerializedName;
+import dev.sbs.api.collection.concurrent.Concurrent;
 import dev.sbs.api.collection.concurrent.ConcurrentList;
 import dev.sbs.api.collection.concurrent.ConcurrentMap;
 import dev.sbs.api.data.Model;
@@ -8,6 +9,17 @@ import dev.sbs.minecraftapi.MinecraftApi;
 import org.jetbrains.annotations.NotNull;
 
 public interface Minion extends Model {
+
+    @NotNull ConcurrentList<Integer> UNIQUE_CRAFTS = Concurrent.newList(
+        0, 5, 15, 30, 50, 75,
+        100, 125, 150, 175, 200, 225, 250, 275, 300, 350,
+        400, 450, 500, 550, 600, 650, 700, 750, 800, 850
+    );
+
+    @NotNull ConcurrentList<Integer> PET_SCORE = Concurrent.newList(
+        10, 25, 50, 75, 100, 130, 175,
+        225, 275, 325, 375, 450, 500
+    );
 
     @NotNull String getId();
 
